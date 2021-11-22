58be92b191c217865bc810c898abdca11b7e25b2
Pieter Nijs <pieternijs@live.be>
2021-11-22 15:05:55 +0100

    Repro

---
+++ src/ReproAnimationWindowsBug/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using System;
+
+namespace ReproAnimationWindowsBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClicked(object sender, EventArgs e)
+        {
+            Animate(lbl);
+            Animate(box);
+            Animate(grid);
+            Animate(btn);
+        }
+
+        private void Animate(VisualElement element)
+        {
+            new Animation
+            {
+                { 0, 1, new (v => element.BackgroundColor = 
+                new Color(element.BackgroundColor.Red, element.BackgroundColor.Green, 
+                (float)v, element.BackgroundColor.Alpha),
+                    element.BackgroundColor.Blue, 1) }
+            }
+            .Commit(element, "test", 16, 250, Easing.Linear, (d, b) => { });
+        }
+    }
+}
